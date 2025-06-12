e3464a6473b6b4b101a0401376f8e077367b8f09
julian343-hue <julian.figjara@gmail.com>
2025-06-12 18:59:45 -0500

    Update catch_up.py
    mejora a los comandos

--- catch_up.py
+++ catch_up.py
@@ -36,13 +36,17 @@ class Enemy(GameSprite):
                self.rect.x += self.speed
 
 class Wall(sprite.Sprite):
-    def __init__(self. color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_hei):
+    def __init__(self, color_1, color_2, color_3, wall_x, wall_y, wall_width, wall_height):
         super().__init__()
-        self.color1 = color1
+        self.color_1 = color_1
+        self.color_2 = color_2
+        self.color_3 = color_3
         self.width = wall_width
-        self.height = player_x
-        self.image = Surface((self.width, self.height))
+        self.height = wall_height
+        
+        self.image = Surface([self.width, self.height])
         self.image.fill((color_1, color_2, color_3))
+        
         self.rect = self.image.get_rect()
         self.rect.x = wall_x
         self.rect.y = wall_y
@@ -66,13 +70,22 @@ packman = Player('hero.png', 5, win_height - 80, 4)
 monster = Enemy('cyborg.png', win_width - 80, 280, 2)
 final = GameSprite('treasure.png', win_width - 120, win_height - 80, 0)
 
-w1 = Wall(154, 285, 50, 100, 20, 450, 10)
-w2 = Wall(154, 285, 50, 100, 400, 350, 10)
-w3 = Wall(154, 285, 50, 100, 20, 10, 300)
+w1 = Wall(154, 205, 50, 100, 20, 450, 10)
+w2 = Wall(154, 205, 50, 100, 400, 350, 10)
+w3 = Wall(154, 205, 50, 100, 20, 10, 380)
+w4 = Wall(154, 205, 50, 200, 130, 10, 350)
+w5 = Wall(154, 205, 50, 450, 130, 10, 360)
+w6 = Wall(154, 205, 50, 300, 20, 350, 10)
+w7 = Wall(154, 205, 50, 390, 120, 130, 10)
 
 game = True
 clock = time.Clock()
 FPS = 60
+
+font.init()
+font = font.Font(None, 70)
+win = font.render("YOU WIN", True, (255, 215, 0))
+lose = font.render("YOU LOSE", True, (100, 0, 0))
 
 
 mixer.init()
@@ -94,6 +107,24 @@ while game:
         monster.reset()
         final.reset()
 
+        w1.draw_wall()
+        w2.draw_wall()
+        w3.draw_wall()
+        w4.draw_wall()
+        w5.draw_wall()
+        w6.draw_wall()
+        w7.draw_wall()
+
+    if sprite.collide_rect(packman, monster) or sprite.collide_rect(packman, w1) or sprite.collide_rect(packman, w2) or sprite.collide_rect(packman, w3)  or sprite.collide_rect(packman, w4)  or sprite.collide_rect(packman, w5)  or sprite.collide_rect(packman, w6)  or sprite.collide_rect(packman, w6)  or sprite.collide_rect(packman, w7):
+        finish = True
+        window.blit(lose, (200,200))
+        kick.play()
+
+    if sprite.collide_rect(packman, final):
+        finish = True
+        window.blit(win, (200, 200))
+        kick.play()
+
+
     display.update()
     clock.tick(FPS)
-    
